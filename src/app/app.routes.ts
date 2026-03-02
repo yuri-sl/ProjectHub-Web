45f54d7faa61de3055d51e04b003c99b9e23cc28
Yuri Santana <yuri03ysl@gmail.com>
2026-03-01 21:06:41 -0300

    Feat: Criando Home e Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
+import { App } from './app';
 
+//Aqui são definidas as rotas globais da aplicação.
 export const routes: Routes = [
     {
         path:'login',
-        component:Login,
-        pathMatch:'full'
-    }
+        component:Login
+    },
+    { path: '', loadChildren: () => import('./template/template-module').then(m => m.TemplateModule) },
+    { path: '**', redirectTo: '' }
 ];
--- src/app/template/template-routing-module.ts
+++ src/app/template/template-routing-module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Login } from '../login/login';
+import { Home } from './pages/home/home';
 
-const routes: Routes = [];
+//Aqui são definidas todas as rotas que obedecem a esse modulo
+const routes: Routes = [
+  {
+    path:'paginas',
+    component:Layout,
+    children: [
+      {path:'home',component:Home}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
